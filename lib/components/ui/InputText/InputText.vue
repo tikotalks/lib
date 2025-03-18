606803190b455c9898b883d578feada90536f631
Sil van Diepen <me@sil.mt>
2025-03-18 11:55:42 +0100

    feat: Enhanced InputText component with CSS variable support for styling

--- lib/components/ui/InputText/InputText.vue
+++ lib/components/ui/InputText/InputText.vue
@@ -26,17 +26,17 @@ defineEmits<{
 <style lang="scss">
 .input-text {
   width: 100%;
-  padding: 0.75em;
+  padding: var(--input-padding,0.75em);
   border: none;
-  border-radius: 0.5em;
-  background: transparent;
-  border: 1px solid var(--color-foreground);
-  color: var(--color-foreground);
-  font-size: 1em;
+  border-radius: var(--input-border-radius, 0.5em);
+  background: var(--input-background, transparent);
+  border: var(--input-border-width, 1px) solid var(--input-border-color, var(--color-foreground));
+  color: var(--input-color, var(--color-foreground));
+  font-size: var(--input-font-size,1em);
 
   &:focus {
     outline: none;
-    border-color: var(--color-primary);
+    border-color: var(--input-border-color--focus, var(--color-primary));
   }
 }
 </style>
