61b32c32defac55d3f12e3499ea25916f6958590
Sil van Diepen <me@sil.mt>
2025-03-18 10:03:50 +0100

    feat: Create useSpeech composable for text-to-speech functionality

---
+++ lib/composables/useSpeech.ts
@@ -0,0 +1,10 @@
+export const useSpeech = () => {
+  const speak = (text: string) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(utterance);
+  };
+
+  return {
+    speak
+  };
+}
