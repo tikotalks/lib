345e54faecd13c61f85d2af6545110aac06b931a
Sil van Diepen <me@sil.mt>
2025-03-18 10:29:30 +0100

    chore: Commented out unused imports and refactored export statements in main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,19 +1,33 @@
-import type { App } from 'vue';
+// import type { App } from 'vue';
 import { Popup, popupService } from './components/ui/Popup';
 import { Button, ButtonGroup } from "./components/ui/Button";
 import { Card } from "./components/ui/Card";
 import { ContextMenu } from "./components/ui/ContextMenu";
 import { InputText } from "./components/ui/InputText";
+import { Icon } from './components/ui/Icon';
+import { useSpeech } from './composables/useSpeech';
+import { useColor } from './composables/useColor';
 
 // Export components
-export { Popup, popupService, Button, ButtonGroup, Card, ContextMenu, InputText };
+export {
+  Popup,
+  popupService,
+  Button,
+  ButtonGroup,
+  Card,
+  ContextMenu,
+  InputText,
+  Icon,
+  useSpeech,
+  useColor
+};
 
 // Export types
 export * from './types';
 
-// Vue plugin installation
-export default {
-  install: (app: App): void => {
-    app.component('TikoPopup', Popup);
-  }
-};
+// // Vue plugin installation
+// export default {
+//   install: (app: App): void => {
+//     app.component('TikoPopup', Popup);
+//   }
+// };
