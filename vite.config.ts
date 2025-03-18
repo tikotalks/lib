45dfb28c4c80e77af1fde77fc3fc591a31d6305f
Sil van Diepen <me@sil.mt>
2025-03-18 10:03:21 +0100

    chore: Enhance Vite configuration with TypeScript support and output settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,43 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      include: ['lib/**/*.ts', 'lib/**/*.vue'],
+      outDir: 'dist',
+      staticImport: true,
+      insertTypesEntry: true,
+      exclude: ['node_modules', 'dist']
+    })
+  ],
+
+
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      name: 'TikoLib',
+      formats: ['es', 'umd'],
+      fileName: (format) => `tiko-lib.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue', 'open-icon', 'bemm'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'bemm':  'bemm',
+          'open-icon': 'OpenIcon'
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, './lib')
     }
   }
 })
