ac0dce936bb8836a28fd6ae15f8382c6a9fa36df
Sil van Diepen <me@sil.mt>
2025-03-18 15:23:28 +0100

    fix: Fixed type definitions in ContextMenu utility functions

--- lib/components/ui/ContextMenu/ContextMenu.utils.ts
+++ lib/components/ui/ContextMenu/ContextMenu.utils.ts
@@ -1,9 +1,9 @@
 import { ContextMenuItemDefault, type ContextMenuItem } from './ContextMenu.model';
 
-export const processMenuItems = (items: Partial<ContextMenuItem>[]): Required<ContextMenuItem>[] => {
+export const processMenuItems = (items: Partial<ContextMenuItem>[]): ContextMenuItem[] => {
 	return items
 		.map((item) => {
-			const newItem = { ...ContextMenuItemDefault, ...item } as Required<ContextMenuItem>;
+			const newItem = { ...ContextMenuItemDefault, ...item } as ContextMenuItem;
 			if (newItem.items && newItem.items.length > 0) {
 				newItem.items = processMenuItems(newItem.items);
 			}
