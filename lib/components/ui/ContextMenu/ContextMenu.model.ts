70a2ebe18ff4b2c393f7782de6cc26d27b34aa72
Sil van Diepen <me@sil.mt>
2025-03-18 09:40:28 +0100

    refactor: Enhanced ContextMenu model with new properties and default values

--- lib/components/ui/ContextMenu/ContextMenu.model.ts
+++ lib/components/ui/ContextMenu/ContextMenu.model.ts
@@ -1,49 +1,29 @@
 import type { Icons } from 'open-icon';
+import { ContextPanelPosition, type ContextPanelConfig, ContextPanelConfigDefault } from '../ContextPanel/ContextPanel.model';
 
 export interface ContextMenuItem {
-  id?: string;
-  disabled?: boolean;
-  icon?: Icons;
-  link?: string;
-  label: string;
-  action?: () => void;
-  children?: ContextMenuItem[];
+	id: string;
+	disabled: boolean;
+	active: boolean;
+	icon: Icons;
+	label: string;
+	link: string;
+	action: (() => void) | undefined;
+	items: Partial<ContextMenuItem>[];
 }
 
-export const ContextMenuPosition = {
-  BOTTOM_CENTER: 'bottom-center',
-  BOTTOM_RIGHT: 'bottom-right',
-  BOTTOM_LEFT: 'bottom-left',
-  TOP_CENTER: 'top-center',
-  TOP_LEFT: 'top-left',
-  TOP_RIGHT: 'top-right',
-  CLICK: 'click'
-} as const;
+export const ContextMenuItemDefault: Required<ContextMenuItem> = {
+	id: '',
+	disabled: false,
+	active: true,
+	icon: '',
+	label: '',
+	link: '',
+	action: undefined,
+	items: [] as Required<ContextMenuItem>[],
+};
 
+export const ContextMenuPosition = ContextPanelPosition;
 export type ContextMenuPosition = typeof ContextMenuPosition[keyof typeof ContextMenuPosition];
-
-export const ContextClickMode = {
-  LONG: 'long',
-  SHORT: 'short',
-  RIGHT: 'right'
-}
-export type ContextClickMode = typeof ContextClickMode[keyof typeof ContextClickMode];
-
-
-export interface ContextMenuConfig {
-  id: string;
-  menu: ContextMenuItem[];
-  clickMode: ContextClickMode;
-  vibrate: boolean;
-  pressTime: number;
-  position: ContextMenuPosition;
-}
-
-export const defaultConfig: ContextMenuConfig = {
-  id: '',
-  menu: [],
-  clickMode: ContextClickMode.SHORT,
-  vibrate: true,
-  pressTime: 750,
-  position: ContextMenuPosition.BOTTOM_CENTER
-}
+export type ContextMenuConfig = ContextPanelConfig;
+export const ContextMenuConfigDefault = ContextPanelConfigDefault;
