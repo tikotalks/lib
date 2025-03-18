7a3497e433de4f488d4126b070a3f1a49a69a1dc
Sil van Diepen <me@sil.mt>
2025-03-18 11:18:20 +0100

    refactor: Refactor Popup service to use new Popup model

--- lib/components/ui/Popup/Popup.service.ts
+++ lib/components/ui/Popup/Popup.service.ts
@@ -1,31 +1,5 @@
-import { ref, markRaw, type Component } from 'vue';
-
-export interface PopupOptions {
-  component: Component;
-  props?: Record<string, any>;
-  title: string;
-  slots?: Record<string, () => Component | string>;
-  onClose?: () => void;
-  config?: {
-    background?: boolean;
-    position?: 'center' | 'bottom' | 'top';
-    canClose?: boolean;
-  };
-}
-
-export interface PopupInstance {
-  id: string;
-  component: Component;
-  title: string;
-  props: Record<string, any>;
-  slots?: Record<string, () => Component | string>;
-  onClose?: () => void;
-  config: {
-    hasBackground: boolean;
-    position: string;
-    canClose: boolean;
-  };
-}
+import { ref, markRaw } from 'vue';
+import { type PopupInstance, type PopupOptions } from './Popup.model';
 
 const usePopupService = () => {
   const popups = ref<PopupInstance[]>([]);
