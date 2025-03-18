9c866cf0305874ad02def508a511ce1ce1361f15
Sil van Diepen <me@sil.mt>
2025-03-18 14:56:53 +0100

    feat: Introduced ContextMenuConfigDefault and ContextMenuItemClass for better context menu structure

--- lib/components/ui/ContextMenu/ContextMenu.model.ts
+++ lib/components/ui/ContextMenu/ContextMenu.model.ts
@@ -27,3 +27,34 @@ export const ContextMenuPosition = ContextPanelPosition;
 export type ContextMenuPosition = typeof ContextMenuPosition[keyof typeof ContextMenuPosition];
 export type ContextMenuConfig = ContextPanelConfig;
 export const ContextMenuConfigDefault = ContextPanelConfigDefault;
+
+
+export const createContextMenuItem = (item: Partial<ContextMenuItem>): ContextMenuItem => {
+	return {
+		...ContextMenuItemDefault,
+		...item,
+		items: item.items ? item.items.map(createContextMenuItem) : [],
+	}
+}
+
+export class ContextMenuItemClass {
+	id: string;
+	disabled: boolean;
+	active: boolean;
+	icon: Icons;
+	label: string;
+	link: string;
+	action: (() => void) | undefined;
+	items: ContextMenuItemClass[];
+
+	constructor(item: Partial<ContextMenuItem>) {
+		this.id = item.id || '';
+		this.disabled = item.disabled || false;
+		this.active = item.active || true;
+		this.icon = item.icon || '';
+		this.label = item.label || '';
+		this.link = item.link || '';
+		this.action = item.action;
+		this.items = item.items ? item.items.map(i => new ContextMenuItemClass(i)) : [];
+	}
+}
